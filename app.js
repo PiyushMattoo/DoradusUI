e859730f037d4235b12b38186c73f1fa4c6390a2
Piyush Mattoo <piyush.mattoo@software.dell.com>
2015-07-15 18:32:16 -0700

    CUI Integration

--- app.js
+++ app.js
@@ -1,57 +1,32 @@
-angular.module('app', ['cui', 'ngRoute'])
+angular.module('app', ['cui'])
 
 .constant('applicationInfo', {
-  name: 'Sitraka Migrator',
-  version: '1.2.3'
+  name: 'Doradus',
+  version: 'v2.4'
 })
 
-.config(function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: 'home.html',
-    controller: 'HomeCtrl'
-  }).when('/pageone', {
-    templateUrl: 'pageone.html',
-    controller: 'PageOneCtrl'
-  });
-})
+angular.module('app', ['cui'])
+  .controller('AppCtrl', function($scope, cuiAboutBox) {
+    var aboutBox = cuiAboutBox({
+      applicationName: 'Change Auditor',
+      licenses: 'licenses.json',
+      thirdParty: 'thirdParty.json',
+      version: '2.0.0',
+      tabs: [{
+        label: 'About',
+        active: true,
+        template: 'aboutBox.about'
+      }]
+    });
 
-.controller('AppCtrl', function($scope, auth, $location) {
-  $scope.auth = auth;
-  $scope.$on('$locationChangeStart', function(event) {
-    if (!auth.isAuthenticated()) {
-      if ($location.path() !== '/login') {
-        $location.path('/login');
-      }
-    } else {
-      if ($location.path() === '/login') {
-        $location.path('/');
-      }
-    }
-  });
-})
+    aboutBox.modal.show();
 
-.controller('StartScreenCtrl', function($scope, applicationInfo, auth) {
-  $scope.startScreenSettings = {
-    applicationName: applicationInfo.name
-  }
-  $scope.onSignIn = function(userData) {
-    auth.login(userData.username, userData.password);
-  }
-})
+    $scope.showAboutBox = aboutBox.modal.show;
+  })
 
-.controller('AppFrameCtrl', function($scope, applicationInfo) {
-  $scope.applicationInfo = applicationInfo;
-  $scope.navItems = [{
-    label: 'Home',
-    icon: 'home',
-    href: '#/'
-  }, {
-    label: 'Page one',
-    icon: 'rocket',
-    href: '#/pageone'
-  }];
-})
+
+
+
 
 .controller('AboutBoxCtrl', function($scope, cuiAboutBox, applicationInfo) {
   var about = cuiAboutBox({
@@ -59,42 +34,6 @@ angular.module('app', ['cui', 'ngRoute'])
     version: applicationInfo.version
   });
   $scope.showAboutBox = about.modal.show;
-})
-
-.controller('HomeCtrl', function() {})
-.controller('PageOneCtrl', function() {})
-
-
-
-.constant('sessionStorageUsername', 'username')
-
-.factory('auth', function($location, sessionStorageUsername) {
-  return {
-    login: function(thisUser, thisPassword) {
-      sessionStorage.setItem(sessionStorageUsername, thisUser);
-      $location.path('/');
-    },
-    isAuthenticated: function() {
-      return !!sessionStorage.getItem(sessionStorageUsername);
-    },
-    getUsername: function() {
-      return sessionStorage.getItem(sessionStorageUsername);
-    },
-    logout: function() {
-      sessionStorage.removeItem(sessionStorageUsername);
-      $location.path('/login');
-    }
-  }
-})
-
-.directive('logout', function(auth) {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs) {
-      element.on('click', function(e) {
-        auth.logout();
-        e.preventDefault();
-      })
-    }
-  }
 });
+
+
